d95952d6471d479504e4abe71ccc265f332b20e2
Kendrick Dubuisson <kedubuis@microsoft.com>
2020-11-12 12:28:58 -0600

    first commit

---
+++ src/main/java/azure/lab/springwebapp/SpringwebappApplication.java
@@ -0,0 +1,13 @@
+package azure.lab.springwebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringwebappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringwebappApplication.class, args);
+	}
+
+}
